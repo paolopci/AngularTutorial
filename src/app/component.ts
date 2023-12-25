a7b40f5a2f182581c2daba04dd2111fbc8ed5a0d
PaoloPci <paolopci@yahoo.it>
2023-12-25 11:41:05 +0100

    [ngClasses],[class.myclass] etc etc

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ApplicationRef} from "@angular/core";
 import {Model} from "./repository.model";
+import {Product} from './product.model'
+
 
 @Component({
   selector: "app",
@@ -8,6 +10,25 @@ import {Model} from "./repository.model";
 export class ProductComponent {
   model: Model = new Model();
 
+  constructor(ref: ApplicationRef) {
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
+  }
+
+
+  FontSizeWithUnits: string = "30px";
+  FontSizeWithOutUnits: string = "30";
+
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
+  }
+
+  getClassesByPosition(position: number): string {
+    let product = this.getProductByPosition(position);
+    return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
+  }
+
+
   getClasses(key: number): string {
     let product = this.model.getProduct(key);
 
@@ -15,4 +36,24 @@ export class ProductComponent {
 
     //return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
   }
+
+  getClassMap(key: number): Object {
+    let product = this.model.getProduct(key);
+
+    return {
+      "text-center bg-danger": product?.name == "kayak",
+      "bg-info": (product?.price ?? 0) < 50
+    };
+  }
+
+  getStyles(key: number): Object {
+    let product = this.model.getProduct(key);
+    return {
+      fontsize: "30px",
+      "margin.px": 100,
+      color: (product?.price ?? 0) > 50 ? "red" : "green"
+    }
+  }
+
+
 }
--- src/app/datasource.mode.ts
+++ src/app/datasource.mode.ts
@@ -7,7 +7,7 @@ export class SimpleDataSource {
     this.data = new Array<Product>(
       new Product(1, "kayak", "Watersports", 275),
       new Product(2, "ball", "soccer", 27.99),
-      new Product(3, "Soccer ball", "soccer", 25.11),
+      new Product(3, "Soccer ball", "soccer", 275.11),
       new Product(4, "kayak", "Watersports", 275),
       new Product(5, "Lifejacket", "Watersports", 48.95),
       new Product(6, "Basket Ball", "Basket", 19.50),
