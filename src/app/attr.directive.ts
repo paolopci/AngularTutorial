5fcff1f9397c009e10cc1d3928d0322dde3cdd71
PaoloPci <paolopci@yahoo.it>
2023-12-29 11:54:45 +0100

    add new files

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,4 +1,5 @@
-﻿import {Directive, ElementRef, Input} from "@angular/core";
+﻿import {Directive, ElementRef, Input, SimpleChanges, Output, EventEmitter} from "@angular/core";
+import {Product} from './product.model'
 
 @Directive({
   selector: "[pa-attr]",
@@ -6,12 +7,32 @@
 export class PaAttrDirective {
 
   constructor(private element: ElementRef) {
+    this.element.nativeElement.addEventListener("click", () => {
+      if (this.product != null) {
+        this.click.emit(this.product);
+      }
+    })
   }
 
   @Input("pa-attr")
   bgClass: string | null = "";
 
-  ngOnInit() {
-    this.element.nativeElement.classList.add(this.bgClass || "table-success", "fw-bold");
+  @Input("pa-product")
+  product: Product = new Product();
+
+  @Output("pa-category")
+  click = new EventEmitter<Product>();
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    let change = changes["bgClass"];
+    let classList = this.element.nativeElement.classList;
+    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+      classList.remove(change.previousValue);
+    }
+
+    if (!classList.contains(change.currentValue)) {
+      classList.add(change.currentValue);
+    }
   }
 }
