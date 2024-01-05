5a12c4d6e0266a1373e6f81609118d2147d0f67d
PaoloPci <paolopci@yahoo.it>
2024-01-05 14:19:06 +0100

    aggiornamento progetto cap 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,8 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {ProductComponent} from "./component";
-import {ProductComponent02} from "./component02";
-import {ProductComponent03} from "./component03";
-import {ProductComponent04} from "./component04";
-import {ProductComponent05} from "./component05";
-import {ProductComponent06} from "./component06";
-import {ProductComponent1301} from "./component1301";
-import {ProductComponent1302} from "./component1302";
-import {ProductComponent1303} from "./component1303";
-import {ProductComponent1401} from "./component1401";
-import {ProductComponent1402} from "./component1402";
-import {ProductComponent1403} from "./component1403";
-import {ProductComponent1501} from "./component1501";
-import {ProductComponent1601} from "./component1601";
+
 import {ProductComponent1701} from "./component1701";
-import {ProductTableComponent} from "./productTable.component";
-import {ProductTableComponent1602} from "./productTable.component1602";
-import {ProductTableComponent1603} from "./productTable.component1603";
-import {ProductTableComponent1604} from "./productTable.component1604";
-import {ProductTableComponent1605} from "./productTable.component1605";
 import {ProductTableComponent1701} from "./productTable.component1701";
 import {ProductFormComponent} from "./productForm.component";
 import {PaToggleView} from "./toggleView.component";
@@ -58,12 +41,9 @@ registerLocaleData(localIt);
 // -------------------------------------------------------------
 @NgModule({
   declarations: [
-    AppComponent, ProductComponent, ProductComponent02, ProductComponent03, ProductComponent04, ProductComponent1401,
-    ProductComponent05, ProductComponent06, ProductComponent1301, PaAttrDirective, ProductComponent1302, ProductComponent1403,
-    ProductComponent1303, PaModel, ExtendDirective, PaStructureDirective, ProductComponent1402, PaIteratorDirective, PaCellColor,
-    PaCellColorSwitcher, ProductComponent1501, ProductComponent1501, ProductTableComponent, ProductFormComponent, PaToggleView,
-    ProductComponent1601, PaAddTaxPipe, PaCategoryFilterPipe, ProductTableComponent1602, ProductTableComponent1603, ProductTableComponent1604
-    , ProductTableComponent1605, ProductTableComponent1701, ProductComponent1701, PaDiscountEditorComponent, PaDiscountDisplayComponent,
+    AppComponent, ProductComponent, PaAttrDirective, PaModel, ExtendDirective, PaStructureDirective, PaIteratorDirective, PaCellColor,
+    PaCellColorSwitcher, ProductFormComponent, PaToggleView, PaAddTaxPipe, PaCategoryFilterPipe, ProductTableComponent1701,
+    ProductComponent1701, PaDiscountEditorComponent, PaDiscountDisplayComponent,
     PaDiscountPipe, PaDiscountAmountDirective
   ],
   imports: [
