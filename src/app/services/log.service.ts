6dead529bc75e41c5536acbae5032b98bea3ce0a
PaoloPci <paolopci@yahoo.it>
2024-01-05 15:17:13 +0100

    add LogServiceMessage in project

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,33 @@
+﻿import {Injectable} from "@angular/core";
+
+
+export enum LogLevel {
+  DEBUG, INFO, ERROR
+}
+
+
+@Injectable()
+
+export class LogService {
+
+  minumumLevel: LogLevel = LogLevel.INFO;
+
+  logInfoMessage(message: string) {
+    this.logMessage(LogLevel.INFO, message);
+  }
+
+  logDebugMessage(message: string) {
+    this.logMessage(LogLevel.DEBUG, message);
+  }
+
+  logErrorMessage(message: string) {
+    this.logMessage(LogLevel.ERROR, message);
+  }
+
+
+  logMessage(level: LogLevel, message: string) {
+    if (level >= this.minumumLevel) {
+      console.log(`Message (${LogLevel[level]}): ${message}`);
+    }
+  }
+}
